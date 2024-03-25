f4e28028d5e91fda96a7fb08e1cfd7db7530fee8
filipvlkous <Thiefcz@email.cz>
2024-03-25 09:36:54 +0100

    add coments

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -15,36 +15,59 @@ import { RootState } from "../Redux/Store";
 import { useRef, useState } from "react";
 
 const variants = ["Marketing", "It", "Sales", "Managment", "Accounting"];
+/**
++ * Component that renders the form for adding new visitors
++ * @returns {JSX.Element} The form component
++ */
 export default function Form() {
+  // reference to form element
   const formRef = useRef<HTMLFormElement>(null);
+  // array of existing visitors from redux store
   const nbRows = useSelector((state: RootState) => state.rows);
+  // function to dispatch actions to redux store
   const dispatch = useDispatch();
+  // state that holds the checked value of the checkbox
   const [checked, setChecked] = useState(false);
 
+  /**
+   * Handle form submission by creating a new visitor object and dispatching action to add it to the redux store
+   * @param {React.FormEvent} e Event object
+   */
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    // if the checkbox is not checked, do not submit the form
     if (checked === false) return;
 
     const formEle = e.target as HTMLFormElement;
 
     const formData = new FormData(formEle);
 
+    /**
+     * Generate a unique id for the new visitor
+     * @returns {number} The unique id
+     */
     const generateUniqueId = (): number => {
       const randomNumber = getRandom();
+      // check if the id is unique from existing visitors
       const isUnique = nbRows.every((item) => item.id !== randomNumber);
+      // if the id is unique, return it, otherwise call the function again
       return isUnique ? randomNumber : generateUniqueId();
     };
 
     const uniqueId = generateUniqueId();
 
+    // create a new visitor object with form values and the unique id
     const data: DataObj = {
       id: uniqueId,
       name: formData.get("Name") as string,
       email: formData.get("Email") as string,
       department: formData.get("Department") as string,
     };
+    // dispatch action to add the new visitor to the redux store
     dispatch(addRow(data));
+    // reset the checked state to false
     setChecked(false);
+    // reset the form
     formRef.current?.reset();
   };
 
